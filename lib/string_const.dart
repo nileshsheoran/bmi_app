fc013b635dd0be2eb463a72730e2d72f8ba76ad5
Nilesh Sheoran <nksheoran2002@gmail.com>
2023-09-02 15:31:00 +0530

    create StringConst class

---
+++ lib/string_const.dart
@@ -0,0 +1,7 @@
+class StringConst {
+  static const String tittleText = 'BMI  Calculator';
+  static const String hintFeetText = 'Enter your feet';
+  static const String hintInchText = 'Enter your inch';
+  static const String hintWeightText = 'Enter your weight';
+  static const String elevatedText = 'Calculate';
+}
