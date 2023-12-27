96fb955a4a50d939e6012b9d685b14a60fe6af53
Nilesh Sheoran <nksheoran2002@gmail.com>
2023-12-27 14:43:04 +0530

    unwanted code remove

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: BmiScreen(),
+      home: const BmiScreen(),
     );
   }
 }
